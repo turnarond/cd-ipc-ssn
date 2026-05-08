f09358a49f40762244aedb226557d06603f98dd8
yanchaodong <yanchaodong@outlook.com>
2026-05-08 10:57:55 +0800

    fix(ssn): ssn_cliauto.h 添加 SSN_API 导出宏

--- src/ssn_cliauto.h
+++ src/ssn_cliauto.h
@@ -47,15 +47,15 @@ typedef void (*ssn_client_conn_func_t)(void *arg, ssn_client_auto_t *cliauto, bo
 
 /* SSN client auto create 
  * The callback function will be called in the client auto thread context */
-ssn_client_auto_t *ssn_client_auto_create(ssn_client_msg_handler_t onmsg, void *arg);
+SSN_API ssn_client_auto_t *ssn_client_auto_create(ssn_client_msg_handler_t onmsg, void *arg);
 
 /* SSN client auto delete
  * After this function is called, the `cliauto` object becomes invalid and is not allowed to be used again */
-void ssn_client_auto_delete(ssn_client_auto_t *cliauto);
+SSN_API void ssn_client_auto_delete(ssn_client_auto_t *cliauto);
 
 /* SSN client auto setup
  * The callback function will be called in the client auto thread context */
-bool ssn_client_auto_setup(ssn_client_auto_t *cliauto, ssn_client_conn_func_t onconn, void *arg);
+SSN_API bool ssn_client_auto_setup(ssn_client_auto_t *cliauto, ssn_client_conn_func_t onconn, void *arg);
 
 /* SSN client auto start
  * `server` can be ip:port or service hostname, client auto will automatically use position to query hostname address.
@@ -65,20 +65,20 @@ bool ssn_client_auto_setup(ssn_client_auto_t *cliauto, ssn_client_conn_func_t on
  * `reconn_delay`: When the connection times out or the password is incorrect or the network is disconnected, 
  *                 the retry waiting time, unit: millisecond, the minimum is 20ms.
  * If turbo ping is enable, `keepalive` is recommended to be an integer multiple of `turbo`. */
-bool ssn_client_auto_start(ssn_client_auto_t *cliauto, const char *server, 
+SSN_API bool ssn_client_auto_start(ssn_client_auto_t *cliauto, const char *server, 
                             char * const urls[], int url_cnt, 
                             unsigned int keepalive, unsigned int conn_timeout, unsigned int reconn_delay);
 
 /* SSN client auto stop
  * `ssn_client_auto_start` and `ssn_client_auto_stop` must be called sequentially */
-bool ssn_client_auto_stop(ssn_client_auto_t *cliauto);
+SSN_API bool ssn_client_auto_stop(ssn_client_auto_t *cliauto);
 
 /* SSN client auto get client handle 
  * This handle is only used for communication, 
  * and cannot perform state operations such as connection closing, 
  * otherwise it will destroy the client auto logic.
  * This client handle is valid after `ssn_client_auto_create` and invalid after `ssn_client_auto_delete` */
-ssn_client_t *ssn_client_auto_handle(ssn_client_auto_t *cliauto);
+SSN_API ssn_client_t *ssn_client_auto_handle(ssn_client_auto_t *cliauto);
 
 #ifdef __cplusplus
 }
